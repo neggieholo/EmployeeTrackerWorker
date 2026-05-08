9522af3bf4e5fac651c82213519e0626988ac343
Simon Enaholo <dragsvillestar@gmail.com>
2026-05-08 13:26:55 +0100

    after deployment save

---
+++ modules/LocationModule/android/build/generated/source/buildConfig/release/com/snametech/employeetrackerWorker/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.snametech.employeetrackerWorker;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.snametech.employeetrackerWorker";
+  public static final String BUILD_TYPE = "release";
+}
